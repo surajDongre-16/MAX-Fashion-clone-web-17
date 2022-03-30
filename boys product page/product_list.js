e852c1db7562c255374e6d5ead07bf9a73b1a6ef
surajDongre-16 <surajdongre8@gmail.com>
2022-03-30 19:10:13 +0530

    boys product pages added

---
+++ boys product page/product_list.js
@@ -0,0 +1,193 @@
+boys_Tees=[
+    {
+        img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959944-Multicolour-M-1000010959944-10032022_01-2100.jpg",
+         img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959944-Multicolour-M-1000010959944-10032022_02-2100.jpg",	
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959944-Multicolour-M-1000010959944-10032022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959944-Multicolour-M-1000010959944-10032022_04-2100.jpg",
+          price : 369,
+          name :"MAX Printed Crew Neck T-shirt"
+    },                                                                                                                                                                                              
+
+    {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959926-Yellow-YELLOW-1000010959926-10032022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959926-Yellow-YELLOW-1000010959926-10032022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959926-Yellow-YELLOW-1000010959926-10032022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010959926-Yellow-YELLOW-1000010959926-10032022_04-2100.jpg",
+          price :329,
+          name : "MAX Printed Sleeveless T-shirt"
+    },
+
+    {
+        img_url1 :"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688202-Yellow-YELLOW-1000010688202-29122021_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688202-Yellow-YELLOW-1000010688202-29122021_02-2100.jpg",
+          img_url3 :"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688202-Yellow-YELLOW-1000010688202-29122021_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688202-Yellow-YELLOW-1000010688202-29122021_04-2100.jpg",
+          price : 249,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688411-Green-GREEN-1000010688411-5012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688411-Green-GREEN-1000010688411-5012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688411-Green-GREEN-1000010688411-5012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688411-Green-GREEN-1000010688411-5012022_04-2100.jpg",
+          price : 249,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688345-Green-GREEN-1000010688345-5012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688345-Green-GREEN-1000010688345-5012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688345-Green-GREEN-1000010688345-5012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688345-Green-GREEN-1000010688345-5012022_04-2100.jpg",
+          price :199,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+    {
+        img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010689273-Blue-NAVY-1000010689273-22122021_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010689273-Blue-NAVY-1000010689273-22122021_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010689273-Blue-NAVY-1000010689273-22122021_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010689273-Blue-NAVY-1000010689273-22122021_04-2100.jpg",
+          price :499,
+          name  :"MAX ColorBlocked Polo T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688499-Yellow-YELLOW-1000010688499-5012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688499-Yellow-YELLOW-1000010688499-5012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688499-Yellow-YELLOW-1000010688499-5012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688499-Yellow-YELLOW-1000010688499-5012022_04-2100.jpg",
+          price :499,
+          name  :"MAX Printed Regular Fit T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416026-Red-RED-1000010416026-5012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416026-Red-RED-1000010416026-5012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416026-Red-RED-1000010416026-5012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416026-Red-RED-1000010416026-5012022_04-2100.jpg",
+          price :199,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+      {	
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010849965-Yellow-YELLOW-1000010849965-29122021_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010849965-Yellow-YELLOW-1000010849965-29122021_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010849965-Yellow-YELLOW-1000010849965-29122021_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010849965-Yellow-YELLOW-1000010849965-29122021_04-2100.jpg",
+          price :249 ,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690119-Green-Olive-1000010690119-22122021_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690119-Green-Olive-1000010690119-22122021_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690119-Green-Olive-1000010690119-22122021_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690119-Green-Olive-1000010690119-22122021_04-2100.jpg",
+          price : 199 ,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688587-Blue-BLUE-1000010688587-5012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688587-Blue-BLUE-1000010688587-5012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688587-Blue-BLUE-1000010688587-5012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688587-Blue-BLUE-1000010688587-5012022_04-2100.jpg",
+          price : 449 ,
+          name  :"MAX Printed Round Neck T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690191-Red-RED-1000010690191-29122021_01-2100.jpg ",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690191-Red-RED-1000010690191-29122021_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690191-Red-RED-1000010690191-29122021_03-2100.jpg ",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690191-Red-RED-1000010690191-29122021_04-2100.jpg",
+          price :169 ,
+          name  :"MAX Printed Round Neck T-shirt"
+      },
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693277-Blue-BLUE-1000010693277-22122021_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693277-Blue-BLUE-1000010693277-22122021_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693277-Blue-BLUE-1000010693277-22122021_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693277-Blue-BLUE-1000010693277-22122021_04-2100.jpg",
+          price :349 ,
+          name  :"MAX Printed Crew Neck T-shirt"
+      },
+
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416015-Grey-MELANGE-1000010416015-12012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416015-Grey-MELANGE-1000010416015-12012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416015-Grey-MELANGE-1000010416015-12012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010416015-Grey-MELANGE-1000010416015-12012022_04-2100.jpg",
+          price :199 ,
+          name  :"MAX Printed Round Neck T-shirt"
+      },
+
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693340-Green-GREEN-1000010693340-15122021_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693340-Green-GREEN-1000010693340-15122021_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693340-Green-GREEN-1000010693340-15122021_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010693340-Green-GREEN-1000010693340-15122021_04-2100.jpg",
+          price : 399 ,
+          name  :"MAX Printed Polo T-shirt"
+      },
+
+
+      {
+          img_url1:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688356-Black-BLACK-1000010688356-12012022_01-2100.jpg",
+          img_url2:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688356-Black-BLACK-1000010688356-12012022_02-2100.jpg",
+          img_url3:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688356-Black-BLACK-1000010688356-12012022_03-2100.jpg",
+          img_url4:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010688356-Black-BLACK-1000010688356-12012022_05-2100.jpg",
+          price :199 ,
+          name  :"MAX Printed Round Neck T-shirt"
+      },
+]
+
+boys_Tees.map(function(ele){
+    var box=document.createElement("div");
+    box.style.margin="16px 0px"
+
+    var img=document.createElement("img");
+    img.src=ele.img_url1;
+    img.style.width="100%";
+    img.style.aspectRatio="1/1.2"
+
+    var price_box=document.createElement("div");
+    price_box.setAttribute("class","price_box");
+
+    var price=document.createElement("h3");
+    var rupee_sign=document.createElement("span");
+    rupee_sign.innerHTML=`<i class='fa-solid fa-indian-rupee-sign'></i> `;
+    rupee_sign.style.fontSize="12px"
+    price.append(rupee_sign,ele.price);
+    price.style.margin="8px 0px";
+    price.style.fontWeight="bolder";
+    price.style.fontSize="25px"
+
+
+    var like=document.createElement("div");
+    like.setAttribute("class","like");
+    
+    var like_img=document.createElement("img");
+    like_img.src="https://cdn-icons.flaticon.com/png/128/2901/premium/2901197.png?token=exp=1648635778~hmac=06c456c3dc80772809d7c530a0e931d2";
+    
+    like.append(like_img);
+
+
+    price_box.append(price,like);
+
+
+    var name=document.createElement("p");
+    name.innerText=ele.name;
+    name.style.fontSize="18px";
+    name.style.marginTop="-10px";
+
+    box.append(img,price_box,name);
+
+    document.querySelector("#product_list").append(box);
+})
+
