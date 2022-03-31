c205a28d1c1634ee13a796c611a04a4ce357ecf2
jksingh1504 <boby150499@gmail.com>
2022-03-31 21:45:31 +0530

    data management

--- nav.js
+++
@@ -1,9 +0,0 @@
-var cat1 = document.querySelector("#cat1")
-var cat = document.querySelector("#cat")
-cat1.addEventListener("mouseenter",function(){
-    
-    for(var x = 0; x<9;x++){
-        document.createElement("a")
-
-    }
-})
