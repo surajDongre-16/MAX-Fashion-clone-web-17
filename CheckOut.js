451ac61dec0d3f2017510641cfcfad4db0c5d9f3
sharangahirekar1 <sharangahirekar1@gmail.com>
2022-04-01 20:35:31 +0530

    Checkout page

---
+++ CheckOut.js
@@ -0,0 +1,35 @@
+//Select a shipping method
+document.querySelector("#shipping>div>div").addEventListener("click",)
+
+
+
+
+
+
+
+//Checkout 
+var basketItems = JSON.parse(localStorage.getItem("toBasket")) || [];
+
+var subtotal = 0;
+basketItems.map(function(e){
+    var box = document.createElement("div");
+    box.style.display = "flex";
+    var image = document.createElement("img");
+    image.setAttribute("src", e.img_1 );
+    
+    var details = document.createElement("div");
+    var name = document.createElement("p");
+    name.innerText = e.name;
+    var price = document.createElement("p");
+    price.innerText = `Price : ${e.price}`;
+    details.append(name,price);
+
+    box.append(image,details);
+    subtotal += Number(e.price);
+    document.querySelector("#itemDetails").append(box);
+})
+
+var subTot = document.querySelector("p2");
+subTot.innerText = subtotal;
+var tot = document.querySelector("p4");
+tot.innerText = subtotal;
